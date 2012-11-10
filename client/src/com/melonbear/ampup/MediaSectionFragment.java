d85b8fa83c30ed920ff9de4fb5b4d358fc57c074
Vidhur Vohra <dkzjunk@yahoo.com>
2012-11-10 13:20:02 -0500

    updated tab policy

--- client/src/com/melonbear/ampup/MediaSectionFragment.java
+++ client/src/com/melonbear/ampup/MediaSectionFragment.java
@@ -41,166 +41,165 @@ import android.widget.LinearLayout;
  */
 public class MediaSectionFragment extends Fragment {
 
-	private static final String LOG_TAG = "AudioRecordTest";
-	private static String mFileName = null;
+  private static final String LOG_TAG = "AudioRecordTest";
+  private static String mFileName = null;
 
-	private RecordButton mRecordButton = null;
-	private MediaRecorder mRecorder = null;
+  private RecordButton mRecordButton = null;
+  private MediaRecorder mRecorder = null;
 
-	private PlayButton mPlayButton = null;
-	private MediaPlayer mPlayer = null;
+  private PlayButton mPlayButton = null;
+  private MediaPlayer mPlayer = null;
 
-	private String username = null;
+  private String username = null;
 
-	private void onRecord(boolean start) {
-		if (start) {
-			startRecording();
-		} else {
-			stopRecording();
-		}
-	}
+  private void onRecord(boolean start) {
+    if (start) {
+      startRecording();
+    } else {
+      stopRecording();
+    }
+  }
 
-	private void onPlay(boolean start) {
-		if (start) {
-			startPlaying();
-		} else {
-			stopPlaying();
-		}
-	}
+  private void onPlay(boolean start) {
+    if (start) {
+      startPlaying();
+    } else {
+      stopPlaying();
+    }
+  }
 
-	private void startPlaying() {
-		mPlayer = new MediaPlayer();
+  private void startPlaying() {
+    mPlayer = new MediaPlayer();
 
-		OnCompletionListener completionListener = new OnCompletionListener() {
+    OnCompletionListener completionListener = new OnCompletionListener() {
 
-			public void onCompletion(MediaPlayer mp) {
-				mPlayButton.setText("Start Playing");
-				mPlayButton.mStartPlaying = !mPlayButton.mStartPlaying;
-				Log.i(LOG_TAG, "Finished Playing");
+      public void onCompletion(MediaPlayer mp) {
+        mPlayButton.setText("Start Playing");
+        mPlayButton.mStartPlaying = !mPlayButton.mStartPlaying;
+        Log.i(LOG_TAG, "Finished Playing");
 
-			}
-		};
+      }
+    };
 
-		mPlayer.setOnCompletionListener(completionListener);
-		try {
-			mPlayer.setDataSource(mFileName);
-			mPlayer.prepare();
-			mPlayer.start();
-		} catch (IOException e) {
-			Log.e(LOG_TAG, "prepare() failed");
-		}
-	}
+    mPlayer.setOnCompletionListener(completionListener);
+    try {
+      mPlayer.setDataSource(mFileName);
+      mPlayer.prepare();
+      mPlayer.start();
+    } catch (IOException e) {
+      Log.e(LOG_TAG, "prepare() failed");
+    }
+  }
 
-	private void stopPlaying() {
-		mPlayer.release();
-		mPlayer = null;
-	}
+  private void stopPlaying() {
+    mPlayer.release();
+    mPlayer = null;
+  }
 
-	private void startRecording() {
+  private void startRecording() {
 
-		mRecorder = new MediaRecorder();
-		mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-		mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-		mRecorder.setOutputFile(mFileName);
-		mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+    mRecorder = new MediaRecorder();
+    mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+    mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+    mRecorder.setOutputFile(mFileName);
+    mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
-		try {
-			mRecorder.prepare();
-		} catch (IOException e) {
-			Log.e(LOG_TAG, "prepare() failed");
-		}
+    try {
+      mRecorder.prepare();
+    } catch (IOException e) {
+      Log.e(LOG_TAG, "prepare() failed");
+    }
 
-		mRecorder.start();
-	}
+    mRecorder.start();
+  }
 
-	private void stopRecording() {
-		mRecorder.stop();
-		mRecorder.release();
-		mRecorder = null;
-	}
+  private void stopRecording() {
+    mRecorder.stop();
+    mRecorder.release();
+    mRecorder = null;
+  }
 
-	class RecordButton extends Button {
-		boolean mStartRecording = true;
+  class RecordButton extends Button {
+    boolean mStartRecording = true;
 
-		OnClickListener clicker = new OnClickListener() {
-			public void onClick(View v) {
-				onRecord(mStartRecording);
-				if (mStartRecording) {
-					setText("Stop recording");
-				} else {
-					setText("Start recording");
-				}
-				mStartRecording = !mStartRecording;
-			}
-		};
+    OnClickListener clicker = new OnClickListener() {
+      public void onClick(View v) {
+        onRecord(mStartRecording);
+        if (mStartRecording) {
+          setText("Stop recording");
+        } else {
+          setText("Start recording");
+        }
+        mStartRecording = !mStartRecording;
+      }
+    };
 
-		public RecordButton(Context ctx) {
-			super(ctx);
-			setText("Start recording");
-			setOnClickListener(clicker);
-		}
-	}
+    public RecordButton(Context ctx) {
+      super(ctx);
+      setText("Start recording");
+      setOnClickListener(clicker);
+    }
+  }
 
-	class PlayButton extends Button {
-		boolean mStartPlaying = true;
+  class PlayButton extends Button {
+    boolean mStartPlaying = true;
 
-		OnClickListener clicker = new OnClickListener() {
-			public void onClick(View v) {
-				onPlay(mStartPlaying);
-				if (mStartPlaying) {
-					setText("Stop playing");
-				} else {
-					setText("Start playing");
-				}
-				mStartPlaying = !mStartPlaying;
-			}
-		};
+    OnClickListener clicker = new OnClickListener() {
+      public void onClick(View v) {
+        onPlay(mStartPlaying);
+        if (mStartPlaying) {
+          setText("Stop playing");
+        } else {
+          setText("Start playing");
+        }
+        mStartPlaying = !mStartPlaying;
+      }
+    };
 
-		public PlayButton(Context ctx) {
-			super(ctx);
-			setText("Start playing");
-			setOnClickListener(clicker);
+    public PlayButton(Context ctx) {
+      super(ctx);
+      setText("Start playing");
+      setOnClickListener(clicker);
 
-		}
-	}
+    }
+  }
 
-	public MediaSectionFragment() {
+  public MediaSectionFragment() {
 
-		mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-		String timestamp = new SimpleDateFormat("yyyyMMddhhmm")
-				.format(new Date());
-		// mFileName += "/audio.3gp";
-		mFileName += "/AMPup/" + this.username + "_" + timestamp + ".3gp";
-		Log.i(this.LOG_TAG, mFileName);
-	}
+    mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+    String timestamp = new SimpleDateFormat("yyyyMMddhhmm").format(new Date());
+    // mFileName += "/audio.3gp";
+    mFileName += "/AMPup/" + this.username + "_" + timestamp + ".3gp";
+    Log.i(this.LOG_TAG, mFileName);
+  }
 
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
 
-		LinearLayout ll = new LinearLayout(getActivity());
+    LinearLayout ll = new LinearLayout(getActivity());
 
-		mRecordButton = new RecordButton(getActivity());
-		ll.addView(mRecordButton, new LinearLayout.LayoutParams(
-				ViewGroup.LayoutParams.WRAP_CONTENT,
-				ViewGroup.LayoutParams.WRAP_CONTENT, 0));
-		mPlayButton = new PlayButton(getActivity());
-		ll.addView(mPlayButton, new LinearLayout.LayoutParams(
-				ViewGroup.LayoutParams.WRAP_CONTENT,
-				ViewGroup.LayoutParams.WRAP_CONTENT, 0));
-		return ll;
-	}
+    mRecordButton = new RecordButton(getActivity());
+    ll.addView(mRecordButton, new LinearLayout.LayoutParams(
+        ViewGroup.LayoutParams.WRAP_CONTENT,
+        ViewGroup.LayoutParams.WRAP_CONTENT, 0));
+    mPlayButton = new PlayButton(getActivity());
+    ll.addView(mPlayButton, new LinearLayout.LayoutParams(
+        ViewGroup.LayoutParams.WRAP_CONTENT,
+        ViewGroup.LayoutParams.WRAP_CONTENT, 0));
+    return ll;
+  }
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		if (mRecorder != null) {
-			mRecorder.release();
-			mRecorder = null;
-		}
+  @Override
+  public void onPause() {
+    super.onPause();
+    if (mRecorder != null) {
+      mRecorder.release();
+      mRecorder = null;
+    }
 
-		if (mPlayer != null) {
-			mPlayer.release();
-			mPlayer = null;
-		}
-	}
+    if (mPlayer != null) {
+      mPlayer.release();
+      mPlayer = null;
+    }
+  }
 }
