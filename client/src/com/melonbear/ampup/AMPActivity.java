31a93714c281129d10c7f80a3f9e679221894639
Jarvis Johnson <magicjarvis@gmail.com>
2012-11-10 13:56:15 -0500

    :musical_note: Remove unused imports

--- client/src/com/melonbear/ampup/AMPActivity.java
+++ client/src/com/melonbear/ampup/AMPActivity.java
@@ -1,20 +1,18 @@
 package com.melonbear.ampup;
 
-import android.support.v4.app.ListFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.ListFragment;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class AMPActivity extends FragmentActivity {
