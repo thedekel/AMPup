53fe9b54d29ebc88a2002dbb2562eec038025b9c
Jarvis Johnson <magicjarvis@gmail.com>
2012-11-10 18:46:14 -0500

    :two_women_holding_hands::heavy_exclamation_mark:

---
+++ client/src/com/melonbear/ampup/Lesson.java
@@ -0,0 +1,13 @@
+package com.melonbear.ampup;
+
+public class Lesson {
+  public String mName;
+
+  public Lesson(String name) {
+    mName = name;
+  }
+
+  public String toString() {
+    return mName;
+  }
+}
