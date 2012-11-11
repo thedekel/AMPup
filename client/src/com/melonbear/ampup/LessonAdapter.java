7a85f3a868cc3610d360ef2f00e308dad7805f6f
Jarvis Johnson <magicjarvis@gmail.com>
2012-11-11 06:58:02 -0500

    :parking::o2:parking: :parking::o2:parking::bangbang:

--- client/src/com/melonbear/ampup/LessonAdapter.java
+++ client/src/com/melonbear/ampup/LessonAdapter.java
@@ -1,8 +1,10 @@
 package com.melonbear.ampup;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
@@ -33,10 +35,10 @@ public class LessonAdapter extends BaseAdapter {
 
   public View getView(int position, View convertView, ViewGroup parent) {
     View v;
-    v = convertView != null ? convertView : mInflater.inflate(R.layout.lesson_list_item, null);
+    v = convertView != null ? convertView 
+      : mInflater.inflate(R.layout.lesson_list_item, null);
     TextView tv = (TextView) v.findViewById(R.id.lesson_title);
     tv.setText(getItem(position).toString());
-    
     ((TextView) v.findViewById(R.id.lesson_subtitle)).setText("Test");
     return v;
   }
